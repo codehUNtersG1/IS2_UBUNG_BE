cfa1792153cc1dfdceabc65176d190d3a648aafc
cglaitong <cglaitong@unal.edu.co>
2018-03-15 05:17:23 -0500

    Dependencies

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,6 @@
 class Food < ApplicationRecord
   has_and_belongs_to_many :diets, :join_table => "diets_foods", :class_name => "Diet"
-  has_many :pictures, as: :imageable
+  has_many :pictures, as: :imageable, dependent: :destroy
   validates :description, presence: true, length:  {maximum: 200}
   validates :type, presence: true
   validates :name, presence: true , length:  {maximum: 30}
--- app/models/phyactivity.rb
+++ app/models/phyactivity.rb
@@ -1,7 +1,7 @@
 class Phyactivity < ApplicationRecord
   has_and_belongs_to_many :plans, :join_table => "plans_physical_activities", :class_name => "Plan"
   has_one :place
-  has_many :pictures, as: :imageable
+  has_many :pictures, as: :imageable, dependent: :destroy
   validates :name, presence: true, length:  {maximum: 50}
   validates :description, presence: true, length:  {maximum: 500}
   validates :duration, presence: true
